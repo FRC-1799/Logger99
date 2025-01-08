7adc9c734fe5489ebb42f53a70255f0b17a14304
Fooot <59772241+Fooot-Code@users.noreply.github.com>
2025-01-07 17:12:26 -0700

    Add files via upload

--- src/logger99.py
+++ src/logger99.py
@@ -1,3 +1,26 @@
-import 
+import discord      # gee i wonder
+import json
 
-def main(args):
+from discord.ext import commands
+
+    # JSON: Parse bot.json to Load Authorization Keys
+
+    # JSON: Parse Data.json to load Organization Rules
+
+    # JSON: Parse registry.json to load Users
+
+intents = discord.Intents.default()
+intents.messages = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+    # Event: Bot is ready
+@bot.event
+async def on_ready():
+    print(f'Started Bot :3')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Hello, world!")
+
+    # Start the bot using your bot token
+bot.run(JSON_BOT_APIKEY)
